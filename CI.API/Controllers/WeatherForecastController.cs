5fdf386e56253e0e9823e046a61805d4cb87e4e2
LAPTOP-04LSJ8HS\Peter B Erdachew <erdachewp@gmail.com>
2021-11-17 12:35:09 -0500

    API project is added

---
+++ CI.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        //  GET api/values http://localhost:5000/api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "State 1", "State 2" };
+        }
+        //  GET api/values/4
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<string>> Get(int id)
+        {
+            return  "State 2";
+        }
+        //  POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+               //  PUT api/values/3
+        [HttpPUT("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+    }
+}
